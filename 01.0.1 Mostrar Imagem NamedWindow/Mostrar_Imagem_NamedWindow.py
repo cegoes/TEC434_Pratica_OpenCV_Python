afe504edb647160fc716aa3aa34a296811b4a6b2
cegoes <cegoes@gmail.com>
2021-08-17 10:39:41 -0300

    updates...

---
+++ 01.0.1 Mostrar Imagem NamedWindow/Mostrar_Imagem_NamedWindow.py
@@ -0,0 +1,14 @@
+import cv2
+import sys
+from pathlib import Path
+
+path = Path(sys.path[0])
+caminhoImagem = str(path.parent.absolute()) + '\\Anexos, Imagens e Videos\\len_std.png'
+
+print(caminhoImagem)
+imagem = cv2.imread(caminhoImagem)
+
+cv2.namedWindow('Imagem carregada', cv2.WINDOW_GUI_NORMAL)
+cv2.imshow('Imagem carregada', imagem)
+
+cv2.waitKey(0)
