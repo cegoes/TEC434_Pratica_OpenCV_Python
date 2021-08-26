0688ca16bc6004682ed37f549802731da3a3456d
cegoes <cegoes@gmail.com>
2021-08-26 19:27:55 -0300

    Updates...

---
+++ 03.01 Threshold_TrackBar/Threshold_TrackBar.py
@@ -0,0 +1,63 @@
+import cv2 as cv
+import numpy as np
+import sys
+from pathlib import Path
+
+# Global variables
+src = np.array
+src_gray  = np.array
+dst = np.array
+threshold_value = 0
+threshold_type = 3
+MAX_VALUE = 255
+MAX_TYPE = 4
+MAX_BINARY_VALUE = 255
+
+window_name = "Threshold Demo"
+
+trackbar_type = "Tipo: \n 0: Binary \n 1: Binary Inverted \n 2: Truncate \n 3: To Zero \n 4: To Zero Inverted"
+trackbar_value = "Valor"
+
+def Threshold_Demo(val):
+  ''' 
+  0: Binary
+  1: Binary Inverted
+  2: Threshold Truncated
+  3: Threshold to Zero
+  4: Threshold to Zero Inverted
+  '''
+  threshold_type = cv.getTrackbarPos(trackbar_type, window_name)
+  threshold_value = cv.getTrackbarPos(trackbar_value, window_name)
+  _, dst = cv.threshold( src_gray, threshold_value, MAX_BINARY_VALUE, threshold_type )
+  cv.imshow( window_name, dst )
+
+if __name__ == '__main__':
+  path = Path(sys.path[0])
+  caminhoImagem = str(path.parent.absolute()) + '\\Anexos, Imagens e Videos\\len_std.png'
+
+  # Carrega a imagem
+  src = cv.imread( caminhoImagem )
+
+   # Converte para Cinza
+  src_gray = cv.cvtColor( src, cv.COLOR_BGR2GRAY)
+
+  # Cria uma janela para mostrar os resultados
+  cv.namedWindow( window_name, cv.WINDOW_GUI_EXPANDED )
+
+  # Cria um Trackbar para escolher o tipo do Threshold
+  cv.createTrackbar( trackbar_type,
+                window_name, 3,
+                MAX_TYPE, Threshold_Demo )
+
+  # Cria um Trackbar para escolher o valor do Threshold
+  cv.createTrackbar( trackbar_value,
+                window_name, 0,
+                MAX_VALUE, Threshold_Demo )
+
+  # Chama a função para inicializar
+  Threshold_Demo(0)
+
+  cv.waitKey(0)
+
+
+
---
+++ 03.2 Threshold Adaptativo/Threshold_Adaptativo.py
@@ -0,0 +1,28 @@
+import cv2 as cv
+import sys
+from pathlib import Path
+
+if __name__ == '__main__':
+    path = Path(sys.path[0])
+    caminhoImagem = str(path.parent.absolute()) + '\\Anexos, Imagens e Videos\\FiguraSombra.png'
+
+    image = cv.imread(caminhoImagem, 0)
+    cv.namedWindow("Imagem original")
+    cv.imshow("Imagem original", image)
+    image = cv.blur(image,(3,3))
+    '''
+    @param src Source 8-bit single-channel image.
+    @param dst Destination image of the same size and the same type as src.
+    @param maxValue Non-zero value assigned to the pixels for which the condition is satisfied
+    @param adaptiveMethod Adaptive thresholding algorithm to use, see #AdaptiveThresholdTypes. The #BORDER_REPLICATE | #BORDER_ISOLATED is used to process boundaries.
+    @param thresholdType Thresholding type that must be either #THRESH_BINARY or #THRESH_BINARY_INV, see #ThresholdTypes.
+    @param blockSize Size of a pixel neighborhood that is used to calculate a threshold value for the pixel: 3, 5, 7, and so on.
+    @param C Constant subtracted from the mean or weighted mean (see the details below). Normally, it is positive but may be zero or negative as well.
+    '''
+    thresholdadap = cv.adaptiveThreshold(image, 255, cv.ADAPTIVE_THRESH_MEAN_C, cv.THRESH_BINARY, 15, -3)
+    cv.namedWindow('Resultado threshold adaptativo')
+    cv.imshow('Resultado threshold adaptativo', thresholdadap)
+    _, thresholdnormal = cv.threshold(image, 25, 255, cv.THRESH_BINARY)
+    cv.namedWindow('Resultado threshold')
+    cv.imshow('Resultado threshold', thresholdnormal)
+    cv.waitKey(0)
