b85f7ef89542de81d021b2f84300e5f242de8441
cegoes <cegoes@gmail.com>
2021-08-27 17:29:09 -0300

    Update HoughCircles.py

--- Hough Circles/HoughCircles.py
+++ Hough Circles/HoughCircles.py
@@ -12,6 +12,7 @@ gray_img = cv2.medianBlur(gray_img, 5)
 circles = cv2.HoughCircles(gray_img,cv2.HOUGH_GRADIENT,1,120,
 param1=100,param2=30,minRadius=0,maxRadius=0)
 circles = np.uint16(np.around(circles))
+
 for i in circles[0,:]:
     # draw the outer circle
     cv2.circle(planets,(i[0],i[1]),i[2],(0,255,0),2)
