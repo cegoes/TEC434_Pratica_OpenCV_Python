9f01038b178e0e79dd5b9455607011e61bb2c36c
cegoes <cegoes@gmail.com>
2021-08-27 11:13:12 -0300

    Updates...

--- 02.16 Detector Bordas Canny/Detector_Bordas_Canny.py
+++ 02.16 Detector Bordas Canny/Detector_Bordas_Canny.py
@@ -5,9 +5,9 @@ from pathlib import Path
 path = Path(sys.path[0])
 caminhoImagem = str(path.parent.absolute()) + '\\Anexos, Imagens e Videos\\pecas_lego.jpg'
 
-imagem = cv2.imread(caminhoImagem)
+imagem = cv2.imread(caminhoImagem, cv2.IMREAD_GRAYSCALE)
 
-imagembordas = cv2.Canny(imagem,30,150)
+imagembordas = cv2.Canny(imagem, 30, 150)
 
 cv2.imshow('Imagem carregada', imagem)
 cv2.imshow('Imagem bordas detectadas', imagembordas)
