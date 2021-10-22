8a577188214b51702b8dd1735f95267d5804ce62
cegoes <cegoes@gmail.com>
2021-10-22 13:20:47 -0300

    Updates

--- 07.05 DFT Passa Baixa/DFT_Passa_Baixa.py
+++ 07.05 DFT Passa Baixa/DFT_Passa_Baixa.py
@@ -6,10 +6,13 @@ import matplotlib.pyplot as plt
 img = cv2.imread('.\\Anexos, Imagens e Videos\\len_std.png',0)
 
 img_float = np.float32(img)
+
 # Step 3: Fourier transform with cv2.dft
 dft = cv2.dft(img_float, flags=cv2.DFT_COMPLEX_OUTPUT)
+
 # Step 4: use np.fft.fftshift to transfer the low frequency to the image center
 dft_center = np.fft.fftshift(dft)
+
 # Step 5: define mask: the generated mask is 1 in the middle and 0 around
 crow, ccol = int(img.shape[0] / 2), int(img.shape[1] / 2) # Find the center of the image
 mask = np.zeros((img.shape[0], img.shape[1], 2), np.uint8)
@@ -29,8 +32,8 @@ img_idf = cv2.magnitude(img_idf[:, :, 0], img_idf[:, :, 1])
 
 # Converte a matrix novamente para o tipo unsigned 8bits e ajusta a escala dos pixels
 cv2.normalize(img_idf, img_idf, 0, 1, cv2.NORM_MINMAX)
-cv2.imshow('Input Image',img)
 
+cv2.imshow('Input Image',img)
 cv2.imshow('Ringed Image',img_idf)
 
 cv2.waitKey(0)
--- _03.04 Threshold Otsu Small Dot Problem/Threshold_Otsu_Small_Dot_Problem.py
+++ _03.04 Threshold Otsu Small Dot Problem/Threshold_Otsu_Small_Dot_Problem.py
@@ -3,6 +3,8 @@ from matplotlib import pyplot as plt
 import sys
 from pathlib import Path
 
+
+
 path = Path(sys.path[0])
 caminhoImagem = str(path.parent.absolute()) + '\\Anexos, Imagens e Videos\\Fig1041(a)(septagon_small_noisy_mean_0_stdv_10).tif'
 
@@ -11,11 +13,14 @@ img = cv.imread(caminhoImagem, 0)
 ret2,th1 = cv.threshold(img, 0, 255, cv.THRESH_BINARY+cv.THRESH_OTSU)
 # Otsu's thresholding after Gaussian filtering
 blur = cv.blur(img,(5,5))
-ret2,th2 = cv.threshold(blur,0,255,cv.THRESH_BINARY+cv.THRESH_OTSU)
+ret2,th2 = cv.threshold(blur, 0, 255,cv.THRESH_BINARY+cv.THRESH_OTSU)
 # plot all the images and their histograms
+
+lblur = cv.Laplacian(blur, cv.CV_8U, ksize=5)
+
 images = [img,  0, th1,
           blur, 0, th2,
-          blur, 0, th2,
+          lblur, 0, th2,
           blur, 0, th2]
 titles = ['Original Noisy Image','Histogram',"Otsu's Thresholding",
           'Blured Noisy Image','Histogram',"Otsu's Thresholding",
