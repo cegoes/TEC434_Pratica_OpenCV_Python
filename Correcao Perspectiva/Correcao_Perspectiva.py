9b4a70ec56818390cedadb58479db05f5cc51050
cegoes <cegoes@gmail.com>
2021-11-05 15:09:12 -0300

    Updates

---
+++ Correcao Perspectiva/Correcao_Perspectiva.py
@@ -0,0 +1,25 @@
+import cv2
+import sys
+import numpy as np
+from pathlib import Path
+
+path = Path(sys.path[0])
+caminhoImagem = str(path.parent.absolute()) + '\\Anexos, Imagens e Videos\\tennis.png'
+
+imagem = cv2.imread(caminhoImagem)
+
+cv2.namedWindow('Imagem Entrada',cv2.WINDOW_GUI_EXPANDED)
+cv2.imshow('Imagem Entrada', imagem)
+
+
+p = np.float32([(356,218),(930,216),(1112,548),(158,552)])
+q = np.float32([(0,0),(954,0),(954,2067),(0,2067)])
+
+rot = cv2.getPerspectiveTransform(p,q)
+
+result = cv2.warpPerspective(imagem, rot, (954,2067))
+
+cv2.namedWindow('Perspectiva Corrigida',cv2.WINDOW_GUI_EXPANDED)
+cv2.imshow('Perspectiva Corrigida', result)
+
+cv2.waitKey(0)
