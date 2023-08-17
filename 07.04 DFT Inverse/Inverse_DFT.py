e03a5ae806fe2bfe6ab241a8b6feee5eb28c1d15
Cláudio Eduardo Góes <cegoes@gmail.com>
2023-08-17 20:06:00 -0300

    Fix

--- 07.04 DFT Inverse/Inverse_DFT.py
+++ 07.04 DFT Inverse/Inverse_DFT.py
@@ -12,16 +12,16 @@ dft = cv2.dft(np.float32(img), flags=cv2.DFT_COMPLEX_OUTPUT)    # Applying DFT
 
 # Devolve novamente a imagem para o domínio do espaço
 img_back = cv2.idft(dft, cv2.DFT_SCALE | cv2.DFT_REAL_OUTPUT )
-#img_back = cv2.magnitude(img_back[:, :, 0], img_back[:, :, 1])
+img_back = cv2.magnitude(img_back[:, :, 0], img_back[:, :, 1])
 
 # Converte a matrix novamente para o tipo unsigned 8bits e ajusta a escala dos pixels
-img_back = np.uint8(img_back/255)
+cv2.normalize(img_back, img_back, 0, 1, cv2.NORM_MINMAX)
 
 # Mostra a imagem original
 cv2.imshow("Original Image", img)
 
 # show the image
-cv2.imshow("Resultado Image", (img_back))
+cv2.imshow("Resultado Image", img_back)
 
 cv2.waitKey()
 cv2.destroyAllWindows()
